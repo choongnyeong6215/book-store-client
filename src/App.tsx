06ea69f2e73be5f804cb8e14c171c39c59da6581
choongnyeong6215 <choong6215@gmail.com>
2024-05-30 17:11:00 +0900

    Clean up components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
 
 const App = () => {
   return (
     <>
       <BookStoreThemeProvider>
-        <ThemeSwitcher />
         <Layout>
           <Home />
         </Layout>
